28536c30e33a7cbacaced4d35b9f3cd77a467d1f
m-ayush-2004 <ayushgupta6194@gmail.com>
2024-01-26 11:31:55 +0530

    prodinno ros file merge and structure merge

---
+++ ros_prodinno/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/ubuntu/Desktop/ros_prodinno/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/ubuntu/Desktop/ros_prodinno/devel;/home/ubuntu/Desktop/project_packages/devel;/home/ubuntu/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/ubuntu/Desktop/ros_prodinno/devel;/home/ubuntu/Desktop/project_packages/devel;/home/ubuntu/catkin_ws/devel;/opt/ros/noetic' != '' else []
